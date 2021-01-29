ce1395bf8478f62753296eac47af645d963d037d
Maicon de Menezes <mdmoliveira@inf.ufpel.edu.br>
2021-01-29 19:38:16 -0300

    teste de imagem

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
+import React from 'react'
+import Image from 'next/image'
 function Home() {
-  return <div>Manifesto SUBurbano</div>
+  return (
+    <div>
+      <Image src="/img/intro.jpg" layout='fill' />      
+    </div>
+  )
 }
 
 export default Home
